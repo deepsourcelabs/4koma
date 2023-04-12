6c3e303c68f4646a7940060e295d3ac1d429e216
raghav-deepsource <71248328+raghav-deepsource@users.noreply.github.com>
2023-04-12 13:34:16 +0530

    fix gradle script

--- build.gradle.kts
+++ build.gradle.kts
@@ -115,7 +115,7 @@ tasks {
     test {
         useJUnitPlatform()
         testLogging.showStandardStreams = true
-        testLogging.exceptionFormat = 'full'
+        testLogging.exceptionFormat = "full"
         finalizedBy(jacocoTestReport)
     }
 
